986cd8e1523b78ebdfe7d1302b86021d051082ca
myamamoto555 <yamamoto@soft.ics.keio.ac.jp>
2018-12-02 13:26:28 +0900

    revise multiple_domains.py

--- multiple_domains.py
+++ multiple_domains.py
@@ -9,47 +9,16 @@ import string
 class RestSpec(DomainSpec):
     name = "restaurant"
     greet = "賃貸住宅検索システムです。"
-    nlg_spec = {"loc": {"inform": ["%sがいいです。", "%s.", "%sに興味があります。", "%sです。", "%sをおねがいします。"],
+    nlg_spec = {"loc": {"inform": ["%sがいいです。", "%s", "%sです。", "%sでおねがいします。"],
                         "request": ["どこらへんがいいですか?", "場所はどこらへんを考えていますか?"]},
-
-                "food_pref": {"inform": ["%sがいいです。", "%sです。", "%sが食べれるレストランがいいです。", "%s。"],
-                              "request": ["何が食べたいですか。", "どんなレストランがいいですか。"]},
-
-                "open": {"inform": ["そのレストランが開いているかは%s。", "ちょうどいま%s"],
-                         "request": ["開いているか教えて.", "何時からやってますか?"],
-                         "yn_question": {'open': ["そのレストランは開いてますか?"],
-                                         'closed': ["閉まってる?"]
-                                         }},
-
-                "parking": {"inform": ["そのレストランの駐車場は%s.", "ここには%s."],
-                            "request": ["駐車場はありますか?.", "車を停めるのは楽ですか?"],
-                            "yn_question": {'street parking': ["道に停めることはできますか?"],
-                                            "valet parking": ["専用の駐車場はありますか?"]
-                                            }},
-
-                "price": {"inform": ["そのレストランの値段は%s", "値段は%s."],
-                          "request": ["平均的な値段は?", "どれくらい高いですか?"],
-                          "yn_question": {'expensive': ["高いですか?"],
-                                          'moderate': ["妥当な値段ですか?"],
-                                          'cheap': ["安いですか?"]
-                                          }},
-
-                "default": {"inform": ["レストラン%sがおすすめです。"],
-                            "request": ["レストランを探しています。",
-                                        "おすすめのレストランは？",
-                                        "レストランを推薦して。"]}
+                "price": {"inform": ["%s以下だと嬉しいです。", "だいたい%sくらいで考えています。"],
+                          "request": ["家賃はどれくらいを想定していますか？"]}
                 }
 
-    usr_slots = [("loc", "location city", ["っｓ", "New York", "Boston", "Seattle",
-                                           "Los Angeles", "San Francisco", "San Jose",
-                                           "Philadelphia", "Washington DC", "Austin"]),
-                 ("food_pref", "food preference", ["Thai", "Chinese", "Korean", "Japanese",
-                                                   "American", "Italian", "Indian", "French",
-                                                   "Greek", "Mexican", "Russian", "Hawaiian"])]
+    usr_slots = [("loc", "location city", ["新宿", "大崎", "品川"]),
+                 ("price", "price range", ["5万円", "8万円", "10万円"])]
 
-    sys_slots = [("open", "if it's open now", ["open", "closed"]),
-                 ("price", "average price per person", ["cheap", "moderate", "expensive"]),
-                 ("parking", "if it has parking", ["street parking", "valet parking", "no parking"])]
+    sys_slots = []
 
     db_size = 100
 
